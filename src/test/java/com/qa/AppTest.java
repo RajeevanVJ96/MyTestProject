e850de6ad6aa295bfa94c54f14e601e2a40df317
RajeevanVJ96 <rvijay46@gmail.com>
2019-08-20 16:43:41 +0100

    Done the second task

--- src/test/java/com/qa/AppTest.java
+++ src/test/java/com/qa/AppTest.java
@@ -1,8 +1,7 @@
 package com.qa;
 
 import static junit.framework.TestCase.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -28,5 +27,31 @@ public class AppTest
         assertFalse("FUckface", app.testFalse(4,2));
     }
 
-    
+    @Test
+    public void testNotNull(){
+        app = new App();
+        assertNotNull("Should return 4", app.testNotNull(2,2));
+    }
+
+    @Test
+    public void testNotSame(){
+        app = new App();
+        assertNotSame("Should be different", app.testNotNull(2,3),app.testNotNull(2,3));
+    }
+
+    @Test
+    public void testBlackJack(){
+        app = new App();
+        assertEquals(0, app.blackJack(23,23));
+        assertEquals(18, app.blackJack(23,18));
+        assertEquals(18, app.blackJack(18,23));
+        assertEquals(18, app.blackJack(18,23));
+        assertEquals(18, app.blackJack(18,23));
+
+
+    }
+
+
+
+
 }
